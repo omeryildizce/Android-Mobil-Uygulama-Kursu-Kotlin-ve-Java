a27802ab6a70ca51ed768bc58b6910cad9f715a6
omeryildizce <omeryildizce@gmail.com>
2023-03-05 22:54:19 +0300

    Create Simpsons.java

---
+++ MethodsAndClasses/app/src/main/java/com/omeryildizce/methodsandclasses/Simpsons.java
@@ -0,0 +1,46 @@
+package com.omeryildizce.methodsandclasses;
+
+public class Simpsons {
+    private String name;
+    private int age;
+    private String job;
+
+    public Simpsons(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Simpsons{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                '}';
+    }
+}
