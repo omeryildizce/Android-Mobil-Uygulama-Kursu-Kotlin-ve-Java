849450fc1fd63280c5f767947ae8510fa56804e4
omeryildizce <omeryildizce@gmail.com>
2023-03-05 14:03:34 +0300

    Create Arrays.java

---
+++ Java/app/src/main/java/com/omeryildizce/java/Arrays.java
@@ -0,0 +1,13 @@
+package com.omeryildizce.java;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] myStringArray = new String[5];
+        myStringArray[0] = "James";
+        myStringArray[1] = "Ömer";
+
+        System.out.println(myStringArray[0]);
+        System.out.println(myStringArray[1]);
+
+    }
+}
