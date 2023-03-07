afba93d664a9c4a84b2a79a804eb91b116abc2ff
omeryildizce <omeryildizce@gmail.com>
2023-03-07 21:18:37 +0300

    Update MainActivity.java

--- s03_Java/CountDownTimer/app/src/main/java/com/omeryildizce/countdowntimer/MainActivity.java
+++ s03_Java/CountDownTimer/app/src/main/java/com/omeryildizce/countdowntimer/MainActivity.java
@@ -2,13 +2,30 @@ package com.omeryildizce.countdowntimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        new CountDownTimer(10_000, 1_000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText(String.format("Left: %d", millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("Left: "+0);
+                textView.setTextColor(Color.RED);
+            }
+        }.start();
     }
 }
