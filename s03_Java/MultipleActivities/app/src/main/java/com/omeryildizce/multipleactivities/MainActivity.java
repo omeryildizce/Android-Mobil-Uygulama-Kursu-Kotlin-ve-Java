21ebf340a9c50a1d2d0538b9926d9540cc1bf3ed
omeryildizce <omeryildizce@gmail.com>
2023-03-07 20:54:53 +0300

    Update MainActivity.java

--- s03_Java/MultipleActivities/app/src/main/java/com/omeryildizce/multipleactivities/MainActivity.java
+++ s03_Java/MultipleActivities/app/src/main/java/com/omeryildizce/multipleactivities/MainActivity.java
@@ -2,13 +2,29 @@ package com.omeryildizce.multipleactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    String username = "Username";
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+    }
+
+    public void changeActivity(View view){
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        if (!editText.getText().toString().matches(""))
+            username = editText.getText().toString();
+        intent.putExtra("username",username);
+        startActivity(intent);
+
     }
 }
