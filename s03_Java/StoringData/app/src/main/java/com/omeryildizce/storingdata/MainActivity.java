819b1bae67e65380a3d164d933555514a34844aa
omeryildizce <omeryildizce@gmail.com>
2023-03-07 19:13:33 +0300

    Shared prefences

--- s03_Java/StoringData/app/src/main/java/com/omeryildizce/storingdata/MainActivity.java
+++ s03_Java/StoringData/app/src/main/java/com/omeryildizce/storingdata/MainActivity.java
@@ -14,32 +14,43 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editTextNumber;
     Button button;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.omeryildizce.storingdata", Context.MODE_PRIVATE);
         setView();
-        int age = 40;
+        int age = sharedPreferences.getInt("age", 0);
+        textView.setText("Your age: " + age);
     }
 
     private void setView() {
         textView = findViewById(R.id.textView);
         editTextNumber = findViewById(R.id.editTextNumber);
         button = findViewById(R.id.buttonKaydet);
+        sharedPreferences = this.getSharedPreferences("com.omeryildizce.storingdata", Context.MODE_PRIVATE);
     }
+
 
     public void save(View view) {
         if (!editTextNumber.getText().toString().matches("")) {
             int age = Integer.parseInt(editTextNumber.getText().toString());
 
             textView.setText("Your age: " + age);
-
+            sharedPreferences.edit().putInt("age", age).apply();
 
 
         }
 
+    }
+
+    public void delete(View view){
+        int age = sharedPreferences.getInt("age",0);
+        if (age != 0){
+            sharedPreferences.edit().remove("age").apply();
+            textView.setText("Your age: " + age);
+        }
     }
 }
