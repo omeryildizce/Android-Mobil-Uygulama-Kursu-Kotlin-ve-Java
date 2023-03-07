1cc739d51d92c2ab265dd9d441a7921afb549d5b
omeryildizce <omeryildizce@gmail.com>
2023-03-07 19:43:56 +0300

    Alert Dialog

--- s03_Java/AlertDialog/app/src/main/java/com/omeryildizce/alertdialog/MainActivity.java
+++ s03_Java/AlertDialog/app/src/main/java/com/omeryildizce/alertdialog/MainActivity.java
@@ -1,14 +1,44 @@
 package com.omeryildizce.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+    }
+
+
+    public void save(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                button.setBackgroundColor(Color.GREEN);
+            }
+        }) ;
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Not Saved", Toast.LENGTH_SHORT).show();
+                button.setBackgroundColor(Color.RED);
+            }
+        });
+        alert.show();
     }
 }
