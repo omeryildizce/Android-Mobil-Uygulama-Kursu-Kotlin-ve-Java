f4617dcaf20aa6ac87b0c547c8c4709a892f4322
omeryildizce <omeryildizce@gmail.com>
2023-03-08 21:49:14 +0300

    Create Hayvan.java

---
+++ s05_OOP/OOPDetails/app/src/main/java/com/omeryildizce/oopdetails/Hayvan.java
@@ -0,0 +1,16 @@
+package com.omeryildizce.oopdetails;
+
+public class Hayvan {
+    String isim;
+    String gozRengi;
+    String tuyRengi;
+
+    public Hayvan() {
+    }
+
+    public Hayvan(String isim, String gozRengi, String tuyRengi) {
+        this.isim = isim;
+        this.gozRengi = gozRengi;
+        this.tuyRengi = tuyRengi;
+    }
+}
