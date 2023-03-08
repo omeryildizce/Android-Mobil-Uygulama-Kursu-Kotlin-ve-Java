23d6f655fb922c62d391a6d715de5e51e9950aa2
omeryildizce <omeryildizce@gmail.com>
2023-03-08 23:09:56 +0300

    Update MainActivity.java

--- s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/MainActivity.java
+++ s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/MainActivity.java
@@ -3,6 +3,9 @@ package com.omeryildizce.landmarkbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.omeryildizce.landmarkbook.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 
@@ -12,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
     Landmark colloseum;
     Landmark londonbridge;
     ArrayList<Landmark> landmarks;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         // Data
         pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
