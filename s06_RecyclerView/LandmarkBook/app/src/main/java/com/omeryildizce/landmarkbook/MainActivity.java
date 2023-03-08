4bc0eb9010f5e859893a67933d841666d85fd54f
omeryildizce <omeryildizce@gmail.com>
2023-03-08 22:57:34 +0300

    Update MainActivity.java

--- s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/MainActivity.java
+++ s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    Landmark pisa;
+    Landmark eiffel;
+    Landmark colloseum;
+    Landmark londonbridge;
+    ArrayList<Landmark> landmarks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Data
+        pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        colloseum = new Landmark("Colloseum", "Italy", R.drawable.colloseum);
+        londonbridge = new Landmark("London Bridge", "England", R.drawable.londonbridge);
+
+        landmarks = new ArrayList<Landmark>();
+        landmarks.add(pisa);
+        landmarks.add(eiffel);
+        landmarks.add(colloseum);
+        landmarks.add(londonbridge);
     }
 }
