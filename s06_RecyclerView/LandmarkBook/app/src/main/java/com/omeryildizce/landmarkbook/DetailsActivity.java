2ffb00ecfc600743bbe12a5037b5083ff3645d03
omeryildizce <omeryildizce@gmail.com>
2023-03-08 22:57:47 +0300

    Update DetailsActivity.java

--- s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/DetailsActivity.java
+++ s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/DetailsActivity.java
@@ -11,6 +11,7 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        // Viewbinding
         
     }
 }
