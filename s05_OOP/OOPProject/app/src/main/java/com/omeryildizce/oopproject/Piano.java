ed411abf2c51f4fdd202df3639d49ddca45f5b0a
omeryildizce <omeryildizce@gmail.com>
2023-03-08 16:57:45 +0300

    Create Piano.java

---
+++ s05_OOP/OOPProject/app/src/main/java/com/omeryildizce/oopproject/Piano.java
@@ -0,0 +1,11 @@
+package com.omeryildizce.oopproject;
+
+public class Piano implements Instrument, HouseDecor{
+    String brand;
+    boolean digital;
+
+    @Override
+    public void info() {
+        System.out.println("Override method");
+    }
+}
