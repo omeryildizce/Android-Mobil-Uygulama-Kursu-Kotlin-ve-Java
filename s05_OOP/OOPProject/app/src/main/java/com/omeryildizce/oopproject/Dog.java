1c5bdbb170d8b817e3cce6887b9f4cdc885da6dd
omeryildizce <omeryildizce@gmail.com>
2023-03-08 16:31:00 +0300

    Create Dog.java

---
+++ s05_OOP/OOPProject/app/src/main/java/com/omeryildizce/oopproject/Dog.java
@@ -0,0 +1,11 @@
+package com.omeryildizce.oopproject;
+
+public class Dog extends Animal{
+    public void test(){
+        super.sing();
+    }
+
+    public void sing(){
+        System.out.println("Dog class");
+    }
+}
