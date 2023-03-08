06e77f35cc3ab2bac4dcc99d81a14b05c31c92e0
omeryildizce <omeryildizce@gmail.com>
2023-03-08 18:54:49 +0300

    Create Kedi.java

---
+++ s05_OOP/OOPDetails/app/src/main/java/com/omeryildizce/oopdetails/Kedi.java
@@ -0,0 +1,25 @@
+package com.omeryildizce.oopdetails;
+
+public class Kedi {
+    private String isim;
+    private String gozRengi;
+    private String tuyRengi;
+
+    public static String turIsmi = "felis";
+    public Kedi() {
+    }
+
+    public Kedi(String isim, String gozRengi, String tuyRengi) {
+        this.isim = isim;
+        this.gozRengi = gozRengi;
+        this.tuyRengi = tuyRengi;
+    }
+
+    public static void konusediStatic(){
+        System.out.println("Benim türüm: " + turIsmi);
+
+    }
+    public void konusedi(){
+        System.out.println("Benim ismim: " + this.isim + "\nBenim türüm: " + turIsmi);
+    }
+}
