eac66f9224c88b31950aeb4b4c737e9424efacbf
omeryildizce <omeryildizce@gmail.com>
2023-03-08 18:54:51 +0300

    Update MainActivity.java

--- s05_OOP/OOPDetails/app/src/main/java/com/omeryildizce/oopdetails/MainActivity.java
+++ s05_OOP/OOPDetails/app/src/main/java/com/omeryildizce/oopdetails/MainActivity.java
@@ -1,14 +1,22 @@
 package com.omeryildizce.oopdetails;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Kedi kedi1 = new Kedi("kedi","sarı", "beyaz");
+        Kedi kedi2 = new Kedi();
+        // turIsmi = "test";
+        // System.out.println(turIsmi);
+        System.out.println(Kedi.turIsmi);
+        kedi1.konusedi();
+        Kedi.konusediStatic();
     }
 }
