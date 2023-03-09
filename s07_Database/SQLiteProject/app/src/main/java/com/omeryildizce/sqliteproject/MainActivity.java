abc654dad9ac2242edc2f699df3a39bda4051f56
omeryildizce <omeryildizce@gmail.com>
2023-03-09 15:00:53 +0300

    Update MainActivity.java

--- s07_Database/SQLiteProject/app/src/main/java/com/omeryildizce/sqliteproject/MainActivity.java
+++ s07_Database/SQLiteProject/app/src/main/java/com/omeryildizce/sqliteproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.omeryildizce.sqliteproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null);
+            database.execSQL("create table if not exists musicians (id integer primary key, name varchar, age int)");
+            //database.execSQL("insert into musicians (name, age) values('Kadir', 35)");
+            //database.execSQL("insert into musicians (name, age) values('Ömer', 25)");
+            //database.execSQL("insert into musicians (name, age) values('Ali', 19)");
+            //database.execSQL("insert into musicians (name, age) values('Ayşe', 32)");
+            //database.execSQL("update musicians set name = 'Fatih' where id = 2 ");
+            // database.execSQL("delete from musicians where id = 2 ");
+            //Cursor cursor = database.rawQuery("select * from musicians where id = 2", null);
+            Cursor cursor = database.rawQuery("select * from musicians", null);
+            int nameIndex = cursor.getColumnIndex("name");
+            int ageIndex = cursor.getColumnIndex("age");
+            int idIndex = cursor.getColumnIndex("id");
+            while (cursor.moveToNext()) {
+                System.out.println("Id:  " + cursor.getInt(idIndex));
+                System.out.println("Name: " + cursor.getString(nameIndex));
+                System.out.println("Age:  " + cursor.getInt(ageIndex));
+            }
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
