dff896a6dcccc912090511f1e7298ec4415e5f0f
omeryildizce <omeryildizce@gmail.com>
2023-03-09 11:57:13 +0300

    Create LandmarkAdapter.java

---
+++ s06_RecyclerView/LandmarkBook/app/src/main/java/com/omeryildizce/landmarkbook/LandmarkAdapter.java
@@ -0,0 +1,60 @@
+package com.omeryildizce.landmarkbook;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.omeryildizce.landmarkbook.databinding.RecyclerRowBinding;
+
+import java.util.ArrayList;
+
+public class LandmarkAdapter extends RecyclerView.Adapter<LandmarkAdapter.LandmarkHolder> {
+    ArrayList<Landmark> landmarks;
+
+
+    public LandmarkAdapter(ArrayList<Landmark> landmarks ) {
+        this.landmarks = landmarks;
+
+    }
+
+    @NonNull
+    @Override
+    public LandmarkHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new LandmarkHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LandmarkHolder holder, int position) {
+        holder.binding.recyclerViewTextView.setText(landmarks.get(position).name);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(), DetailsActivity.class);
+               // intent.putExtra("landmark", landmarks.get(position));
+                Singleton singleton = Singleton.getInstance();
+                singleton.setSentLandmark(landmarks.get(position));
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return landmarks.size();
+    }
+
+    public class LandmarkHolder extends RecyclerView.ViewHolder{
+        private RecyclerRowBinding binding;
+
+        public LandmarkHolder(RecyclerRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
