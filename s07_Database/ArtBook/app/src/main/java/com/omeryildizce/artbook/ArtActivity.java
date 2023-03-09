45dc4c9e470a8c439ebcd0aca68d725bcd2943c8
omeryildizce <omeryildizce@gmail.com>
2023-03-09 16:57:31 +0300

    Update ArtActivity.java

--- s07_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/ArtActivity.java
+++ s07_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/ArtActivity.java
@@ -11,7 +11,10 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -24,6 +27,8 @@ public class ArtActivity extends AppCompatActivity {
     private ActivityArtBinding binding;
     ActivityResultLauncher<Intent> activityResultLauncher;
     ActivityResultLauncher<String> permissionLauncher;
+    Bitmap selectedImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,33 +64,40 @@ public class ArtActivity extends AppCompatActivity {
         }
     }
 
-    private void registerLauncher(){
+    private void registerLauncher() {
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK){
-                        Intent intentFromResult = result.getData();
-                        if (intentFromResult != null){
-                            Uri imageData = intentFromResult.getData();
-                            // binding.imageView.setImageURI(imageData);
-                            try {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        Uri imageData = intentFromResult.getData();
+                        // binding.imageView.setImageURI(imageData);
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
 
-                            }catch (Exception e){
-                                e.printStackTrace();
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
                             }
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                     }
+                }
             }
         });
         permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
             @Override
             public void onActivityResult(Boolean result) {
-                if (result){
+                if (result) {
                     // permission granted
                     Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-                }
-                else {
+                } else {
                     // permission denied
                     Toast.makeText(ArtActivity.this, "Permission needed!", Toast.LENGTH_SHORT).show();
                 }
