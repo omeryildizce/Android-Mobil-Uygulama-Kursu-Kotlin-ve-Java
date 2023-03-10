af364d98c4719590f3c5bc4e7c035f2bec710863
omeryildizce <omeryildizce@gmail.com>
2023-03-10 15:01:19 +0300

    Update MainActivity.java

--- s07_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/MainActivity.java
+++ s07_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/MainActivity.java
@@ -2,8 +2,11 @@ package com.omeryildizce.artbook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,16 +15,44 @@ import android.view.View;
 
 import com.omeryildizce.artbook.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
+    ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        
+        artArrayList = new ArrayList<>();
+        binding.recycleViewer.setLayoutManager(new LinearLayoutManager(this));
+        artAdapter = new ArtAdapter(artArrayList);
+        binding.recycleViewer.setAdapter(artAdapter);
+        getData();
+
+    }
+
+    private void getData() {
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("select * from arts", null);
+            int nameIndex = cursor.getColumnIndex("artname");
+            int idIndex = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(nameIndex);
+                int id = cursor.getInt(idIndex);
+                Art art = new Art(name, id);
+                artArrayList.add(art);
+            }
+            cursor.close();
+            artAdapter.notifyDataSetChanged();
+        } catch (Exception e) {
+
+        }
 
     }
 
@@ -36,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.add_art) {
             Intent intent = new Intent(this, ArtActivity.class);
+            intent.putExtra("info","new");
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
