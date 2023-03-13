3eee17a6f063071a5c3004a84831dd7f5f7ae29f
omeryildizce <omeryildizce@gmail.com>
2023-03-13 19:47:32 +0300

    Create UploadActivity.java

---
+++ s09_InstagramClone/InstagramClone/app/src/main/java/com/omeryildizce/instagramclone/UploadActivity.java
@@ -0,0 +1,20 @@
+package com.omeryildizce.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+    public void uploadButton(View view) {
+    }
+
+    public void selectImage(View view) {
+    }
+}
