99d070cce53e6e6f454840107bb431448448db3a
omeryildizce <omeryildizce@gmail.com>
2023-03-15 18:14:52 +0300

    Update CryptoAPI.java

--- s11_Retrofit/KriptoPare/app/src/main/java/com/omeryildizce/kriptopara/service/CryptoAPI.java
+++ s11_Retrofit/KriptoPare/app/src/main/java/com/omeryildizce/kriptopara/service/CryptoAPI.java
@@ -4,10 +4,12 @@ import com.omeryildizce.kriptopara.model.CryptoModel;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface CryptoAPI {
     @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    Call<List<CryptoModel>> getData();
+    Observable<List<CryptoModel>> getData();
+    //Call<List<CryptoModel>> getData();
 }
