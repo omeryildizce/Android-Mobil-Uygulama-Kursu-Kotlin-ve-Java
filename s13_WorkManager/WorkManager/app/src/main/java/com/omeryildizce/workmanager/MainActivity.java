c1ed3510d3179b0bab222c72c084ee9da28dd049
omeryildizce <omeryildizce@gmail.com>
2023-03-19 22:28:41 +0300

    Update MainActivity.java

--- s13_WorkManager/WorkManager/app/src/main/java/com/omeryildizce/workmanager/MainActivity.java
+++ s13_WorkManager/WorkManager/app/src/main/java/com/omeryildizce/workmanager/MainActivity.java
@@ -1,8 +1,16 @@
 package com.omeryildizce.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Data data = new Data.Builder().putInt("intKey", 1).build();
+        Constraints constraints = new Constraints.Builder()
+                //.setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(false)
+                .build();
+        WorkRequest workRequest = new OneTimeWorkRequest.Builder(RefreshDaatabase.class)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .setInitialDelay(5, TimeUnit.SECONDS)
+                .addTag("myTag")
+                .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
     }
 }
