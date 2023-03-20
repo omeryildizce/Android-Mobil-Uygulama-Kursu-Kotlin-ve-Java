423a9f22623d4429b3df322afeb51e7c17d600ab
omeryildizce <omeryildizce@gmail.com>
2023-03-20 23:11:02 +0300

    Create FruitNinja.java

---
+++ s14_FruitNinja/FruitNinja/core/src/com/omeryildizce/fruitninjastarter/FruitNinja.java
@@ -0,0 +1,29 @@
+package com.omeryildizce.fruitninjastarter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FruitNinja extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("ninjabackground.png");
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
