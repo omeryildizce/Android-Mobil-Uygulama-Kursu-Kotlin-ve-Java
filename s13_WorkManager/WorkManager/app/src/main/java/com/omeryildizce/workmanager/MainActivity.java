ae7ab4dc807eaa9904f7051ad29576554a25df5a
omeryildizce <omeryildizce@gmail.com>
2023-03-20 23:09:32 +0300

    Update MainActivity.java

--- s13_WorkManager/WorkManager/app/src/main/java/com/omeryildizce/workmanager/MainActivity.java
+++ s13_WorkManager/WorkManager/app/src/main/java/com/omeryildizce/workmanager/MainActivity.java
@@ -1,19 +1,24 @@
 package com.omeryildizce.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("EnqueueWork")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 .setRequiresBatteryNotLow(true)
                 .setRequiresCharging(false)
                 .build();
+        /*
         WorkRequest workRequest = new OneTimeWorkRequest.Builder(RefreshDaatabase.class)
                 .setConstraints(constraints)
                 .setInputData(data)
@@ -33,5 +39,43 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         WorkManager.getInstance(this).enqueue(workRequest);
+        */
+
+        WorkRequest workRequest = new PeriodicWorkRequest.Builder(RefreshDaatabase.class, 1, TimeUnit.MINUTES )
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                if (workInfo.getState() == WorkInfo.State.RUNNING){
+                    System.out.println("running");
+                } else if (workInfo.getState() == WorkInfo.State.BLOCKED) {
+                    System.out.println("blocked");
+                    
+                } else if (workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                    System.out.println("succeeded");
+                } else if (workInfo.getState() == WorkInfo.State.FAILED) {
+                    System.out.println("failed");
+                }
+            }
+        });
+        // WorkManager.getInstance(this).cancelAllWork();
+
+        // Chaining
+        /*
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(RefreshDaatabase.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+        */
     }
+
 }
