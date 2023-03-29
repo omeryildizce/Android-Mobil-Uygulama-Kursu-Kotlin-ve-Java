098c0949cce7b113adf0433d3a3e4078b2a55c91
omeryildizce <omeryildizce@gmail.com>
2023-03-29 15:01:17 +0300

    Update Sets.kt

--- s15_KotlinLearning/KotlinLearning/app/src/main/java/com/omeryildizce/kotlinlearning/kotlin/Sets.kt
+++ s15_KotlinLearning/KotlinLearning/app/src/main/java/com/omeryildizce/kotlinlearning/kotlin/Sets.kt
@@ -1,5 +1,20 @@
 package com.omeryildizce.kotlinlearning.kotlin
 
 fun main(args: Array<String>) {
+    val myExampleArray = arrayOf(1,1,2,3,4)
+    println("first element: ${myExampleArray[0]}")
+
+    val mySet = setOf<Int>(1,1,2,3)
+    println(mySet.toString())
+    println(mySet.size)
+
+    for ( value in mySet){
+        print("$value")
+    }
+    println()
+    val myString = HashSet<String>()
+    myString.add("Ömer")
+    myString.add("Ömer")
+    println(myString.toString())
 
 }
