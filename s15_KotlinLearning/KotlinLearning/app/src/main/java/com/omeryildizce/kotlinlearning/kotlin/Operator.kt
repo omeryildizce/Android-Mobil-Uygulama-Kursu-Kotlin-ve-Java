191d2075866fb105eea1f0edaeba789262019885
omeryildizce <omeryildizce@gmail.com>
2023-03-29 15:01:14 +0300

    Create Operator.kt

---
+++ s15_KotlinLearning/KotlinLearning/app/src/main/java/com/omeryildizce/kotlinlearning/kotlin/Operator.kt
@@ -0,0 +1,18 @@
+package com.omeryildizce.kotlinlearning.kotlin
+
+fun main(args: Array<String>) {
+    var m = 5
+    m = m + 1
+    m++
+    m += 1
+    m--
+
+    var n = 9
+    // &&
+    // ||
+
+    println((n > m) && (1 > 2))
+    println(n > m || 1 > 2)
+
+    println()
+}
