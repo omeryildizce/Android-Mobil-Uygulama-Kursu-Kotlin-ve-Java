1b8cbcd0beb50f4d1e7667c17709efe53f97e577
omeryildizce <omeryildizce@gmail.com>
2023-03-29 15:25:18 +0300

    Update MainActivity.kt

--- s15_KotlinLearning/ViewBinding/app/src/main/java/com/omeryildizce/viewbinding/MainActivity.kt
+++ s15_KotlinLearning/ViewBinding/app/src/main/java/com/omeryildizce/viewbinding/MainActivity.kt
@@ -2,10 +2,28 @@ package com.omeryildizce.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.omeryildizce.viewbinding.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root;
+        setContentView(view)
+
+    }
+
+    fun degistir(view: View) {
+        var randInt = Random.nextInt(2)
+        if (randInt == 0){
+            binding.textViewName.setText( "YILDIZ Ömer")
+
+        }else{
+            binding.textViewName.setText( "Ömer YILDIZ")
+
+        }
     }
 }
