4cb0fc714bc2f3a05655fdbdf1c33daa1efdec08
omeryildizce <omeryildizce@gmail.com>
2023-03-29 20:40:50 +0300

    Update MainActivity.kt

--- s16_Kotlin/AlertDialog/app/src/main/java/com/omeryildizce/alertdialog/MainActivity.kt
+++ s16_Kotlin/AlertDialog/app/src/main/java/com/omeryildizce/alertdialog/MainActivity.kt
@@ -2,10 +2,37 @@ package com.omeryildizce.alertdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.omeryildizce.alertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_SHORT).show()
+        save()
+
+    }
+
+    private fun save() {
+        binding.save.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Save")
+            alert.setMessage("Are You Sure?")
+            alert.setPositiveButton("yes"){dialog, which ->
+                Toast.makeText(applicationContext, "Saved", Toast.LENGTH_SHORT).show()
+            }
+            alert.setNegativeButton("no"){dialog, which ->
+                Toast.makeText(applicationContext, "Not Saved", Toast.LENGTH_SHORT).show()
+
+            }
+            alert.show()
+
+        }
     }
 }
