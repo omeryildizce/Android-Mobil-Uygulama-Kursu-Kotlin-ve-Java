76c19c1f0260b4106fe10ea788b8afea13cc906b
omeryildizce <omeryildizce@gmail.com>
2023-03-29 15:01:12 +0300

    Create Nullability.kt

---
+++ s15_KotlinLearning/KotlinLearning/app/src/main/java/com/omeryildizce/kotlinlearning/kotlin/Nullability.kt
@@ -0,0 +1,23 @@
+package com.omeryildizce.kotlinlearning.kotlin
+
+fun main(args: Array<String>) {
+    val myString: String? = null
+    var myAge: Int? = null
+
+    println(myString)
+    println(myAge)
+    // 1 Null safety
+    if (myAge != null) {
+        println(myAge * 10)
+    }else{
+        println(myAge)
+    }
+    // 2 Safe call
+    println(myAge?.compareTo(2))
+
+    // 3 elvis
+    val myResult = myAge?.compareTo(2) ?: -100
+    println(myResult)
+
+    
+}
