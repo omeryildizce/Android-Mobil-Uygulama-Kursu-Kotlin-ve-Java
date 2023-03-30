154d688fa01f95d6e711fd35986c5f465bbba5c1
omeryildizce <omeryildizce@gmail.com>
2023-03-30 21:33:02 +0300

    Update MainActivity.kt

--- s20_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/MainActivity.kt
+++ s20_Database/ArtBook/app/src/main/java/com/omeryildizce/artbook/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.omeryildizce.artbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.omeryildizce.artbook.databinding.ActivityArtBinding
+import com.omeryildizce.artbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.art_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (R.id.add_art_item == item.itemId){
+            val intent = Intent(this, ArtActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
