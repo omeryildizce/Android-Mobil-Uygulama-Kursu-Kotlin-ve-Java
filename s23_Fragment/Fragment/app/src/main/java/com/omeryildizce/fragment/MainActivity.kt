b99a5e1b4515020b7acf1e0efe5146c319aabc5b
omeryildizce <omeryildizce@gmail.com>
2023-04-01 23:37:58 +0300

    Update MainActivity.kt

--- s23_Fragment/Fragment/app/src/main/java/com/omeryildizce/fragment/MainActivity.kt
+++ s23_Fragment/Fragment/app/src/main/java/com/omeryildizce/fragment/MainActivity.kt
@@ -2,10 +2,28 @@ package com.omeryildizce.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.omeryildizce.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFirst.setOnClickListener { showFragment(BlankFragment()) }
+        binding.buttonSeccond.setOnClickListener { showFragment(BlankFragment2()) }
+
+
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(binding.frameLayout.id,fragment).commit()
     }
 }
+
