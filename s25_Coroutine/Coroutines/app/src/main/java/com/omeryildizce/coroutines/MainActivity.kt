3d26e8963cd1d3835f756895c3ddde1bb2526603
omeryildizce <omeryildizce@gmail.com>
2023-04-03 20:44:34 +0300

    Update MainActivity.kt

--- s25_Coroutine/Coroutines/app/src/main/java/com/omeryildizce/coroutines/MainActivity.kt
+++ s25_Coroutine/Coroutines/app/src/main/java/com/omeryildizce/coroutines/MainActivity.kt
@@ -2,10 +2,40 @@ package com.omeryildizce.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        GlobalScope.launch {
+            repeat(100_000) {
+                launch {
+                    println("android")
+                }
+            }
+        }
+            */
+        // Scope
+        // Global Scope, runBlocking ,coroutineScope
+        // run blockig
+        /*
+        println("run blocking start")
+        runBlocking {  launch {
+            delay(2000)
+            println("run blocking")
+        } }
+        println("run blocking end")
+        */
+
+        // Global scope
+        println("global scope start")
+        GlobalScope.launch {
+            delay(5000)
+            println("global scope")
+        }
+        println("global scope end")
     }
 }
