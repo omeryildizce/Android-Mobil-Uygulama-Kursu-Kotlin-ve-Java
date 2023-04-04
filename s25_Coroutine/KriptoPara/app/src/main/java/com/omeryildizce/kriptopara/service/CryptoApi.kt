424691e08149b285a0fa7911cfd921b3b32fb042
omeryildizce <omeryildizce@gmail.com>
2023-04-04 16:38:55 +0300

    Update CryptoApi.kt

--- s25_Coroutine/KriptoPara/app/src/main/java/com/omeryildizce/kriptopara/service/CryptoApi.kt
+++ s25_Coroutine/KriptoPara/app/src/main/java/com/omeryildizce/kriptopara/service/CryptoApi.kt
@@ -4,12 +4,14 @@ import com.omeryildizce.kriptopara.model.CryptoModel
 import io.reactivex.Observable
 import io.reactivex.Observer
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CryptoApi {
     // GET, POST, UPDATE, DELETE
     @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    fun getData() : Observable<List<CryptoModel>>
-// fun getData(): Call<List<CryptoModel>>
+    //fun getData() : Observable<List<CryptoModel>>
+    // fun getData(): Call<List<CryptoModel>>
+     suspend fun getData(): Response<List<CryptoModel>>
 
 }
