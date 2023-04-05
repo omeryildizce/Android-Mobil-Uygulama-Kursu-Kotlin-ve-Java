de5382d506f758005f17abb76a03214a034b9a72
omeryildizce <omeryildizce@gmail.com>
2023-04-05 13:08:52 +0300

    Delete Binance_Key.kt

--- s28_StateManagement/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/binancekey/Binance_Key.kt
+++
@@ -1,6 +0,0 @@
-package com.omeryildizce.cryptocrazy.binancekey
-
-class Binance_Key {
-    private val apiKey = "udziMGxmqPo76dtqbzmLoJY6GPAP70vPdKwq8oKIWdItahuXCHJ8fnOekRyKScL1"
-    private val secretKey = "pFJ9kmMEmR3ayeXVyQTClmQHD8K1Iid8pXTdLwNWhtb4mvBYZpOujdZaPxqr5VP3"
-}
