4bdaea7b8344fcf867d4f56d647ed7bab05f2155
omeryildizce <omeryildizce@gmail.com>
2023-04-05 03:20:58 +0300

    Update MainActivity.kt

--- s28_StateManagement/StateManagementCompose/app/src/main/java/com/omeryildizce/statemanagementcompose/MainActivity.kt
+++ s28_StateManagement/StateManagementCompose/app/src/main/java/com/omeryildizce/statemanagementcompose/MainActivity.kt
@@ -3,126 +3,96 @@ package com.omeryildizce.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.omeryildizce.statemanagementcompose.ui.theme.StateManagementComposeTheme
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.omeryildizce.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             StateManagementComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    MainScreen("Android")
+                    MainScreen()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun MainScreen(name: String) {
-    Surface(color = Color.LightGray) {
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Hello!",
-                fontSize = 26.sp
-            )
-            Button(onClick = { }, colors = ButtonDefaults.buttonColors(Color.Blue)){
-                Text(text = "Hello!" , color = Color.White)
-                Spacer(modifier = Modifier.padding(start = 2.dp))
-                Text(text = "Hi!", color = Color.Magenta)
-            }
-                Spacer(modifier = Modifier.padding(4.dp))
-            Image(painter = painterResource(id = R.drawable.logo), contentDescription ="" , Modifier.size(128.dp) )
-            
-        }
-    }
-
-    // cardDesign()
-}
-
-@Composable
-private fun cardDesign() {
-    var expanded = remember {
-        mutableStateOf(false)
-    }
-    Column(
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Card {
-            Column(
-                modifier = Modifier
-                    .clickable {
-                        expanded.value = !expanded.value
-                    }
-                    .fillMaxSize(0.90f)
-            ) {
-                Image(
-                    painterResource(id = R.drawable.logo),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color.Blue,
-                                    Color.Black
-                                )
-                            )
-                        )
-                        .padding(16.dp),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "",
-
-                    )
-                Spacer(modifier = Modifier.padding(10.dp))
-                AnimatedVisibility(visible = expanded.value) {
-                    Text(
-                        text = "Jetpack Compose",
-                        style = MaterialTheme.typography.h2,
-                        textAlign = TextAlign.Center,
-                        color = Color.Blue
-                    )
-                }
-            }
-
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StateManagementComposeTheme {
-        MainScreen("Android")
+        MainScreen()
     }
+}
+
+
+@Composable
+fun MainScreen() {
+    var myString by remember {
+        mutableStateOf("")
+    }
+    var myPassword by remember {
+        mutableStateOf("")
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.background(Color.Magenta),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SpecialText(string = "Test")
+            Spacer(modifier = Modifier.padding(8.dp))
+            SpecialText(string = "Helo World!")
+            Spacer(modifier = Modifier.padding(8.dp))
+            SpecialTextField(myString){
+                myString = it
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            SpecialTextField(myPassword){
+                 myPassword = it
+            }
+        }
+    }
+}
+
+@Composable
+fun SpecialText(string: String) {
+    Text(
+        text = string,
+        fontSize = 20.sp,
+        fontStyle = FontStyle.Italic,
+        fontFamily = FontFamily.Monospace,
+    )
+}
+
+@Composable
+fun SpecialTextField( myString:String , function: (String)-> Unit){
+
+    TextField(value = myString ,onValueChange = function )
 }
