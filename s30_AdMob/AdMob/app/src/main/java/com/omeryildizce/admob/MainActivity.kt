b77c9c5c19f10ba23f82cf0a79998ce8d4f3cb65
omeryildizce <omeryildizce@gmail.com>
2023-04-06 01:10:19 +0300

    Update MainActivity.kt

--- s30_AdMob/AdMob/app/src/main/java/com/omeryildizce/admob/MainActivity.kt
+++ s30_AdMob/AdMob/app/src/main/java/com/omeryildizce/admob/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.omeryildizce.admob
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+        lateinit var mAdView : AdView
+        var mInterstitialAd: InterstitialAd? = null
+        var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest1 = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest1)
+
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.toString() as String)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
+        // test banner
+        // ca-app-pub-3940256099942544/6300978111
+        //Geçiş reklamı
+        // ca-app-pub-3940256099942544/1033173712
+    }
+
+    fun gotoSecond(view: View) {
+
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
